9b069473a6bc8a4ae1463c47df5c5488e6a9c04b
Avani Garg <142567720+avnigarg12@users.noreply.github.com>
2026-04-04 01:59:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        brand: {
+          dark: '#020617', // slate-950
+          blue: '#2563eb', // blue-600
+          lightgreen: '#22c55e', // green-500
+          teal: '#0d9488', // teal-600
+        }
+      },
+      animation: {
+        'blob': 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': { transform: 'translate(0px, 0px) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0px, 0px) scale(1)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
